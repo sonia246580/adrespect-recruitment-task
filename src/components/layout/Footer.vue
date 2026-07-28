9e8e5bac599f942f38d90b479313e76bf7db0412
sonia246580 <makuchsonia06@gmail.com>
2026-07-28 20:13:56 +0200

    implement responsive footer layout

--- src/components/layout/Footer.vue
+++ src/components/layout/Footer.vue
@@ -1,37 +1,62 @@
 <script setup>
 import BaseButton from '../common/BaseButton.vue'
-import logo from '../../assets/images/logo.svg'
 import BaseContainer from '../common/BaseContainer.vue'
-import adRespactLogo from '../../assets/images/adrespect.svg'
+import logo from '../../assets/images/logo.svg'
+import adRespectLogo from '../../assets/images/adrespect.svg'
 </script>
 
 <template>
     <footer class="bg-[#111111] text-[#F5F0EC]">
         <BaseContainer>
-            <div class="mx-auto w-full max-w-[1040px] py-[72px]">
-                <div class="flex items-center justify-between pb-12">
-                    <img
-                        :src="logo"
-                        alt="GiardDesign"
-                        class="h-auto w-[112px] brightness-0 invert"
-                    />
+            <div class="mx-auto w-full max-w-[1040px] py-12 md:py-14 lg:py-[72px]">
+                <div
+                    class="flex flex-col items-start gap-8 pb-10
+                           md:flex-row md:items-center md:justify-between
+                           md:gap-8 md:pb-12"
+                >
+                    <a
+                        href="/"
+                        aria-label="GiardDesign — strona główna"
+                        class="shrink-0"
+                    >
+                        <img
+                            :src="logo"
+                            alt="GiardDesign"
+                            class="h-auto w-[112px] brightness-0 invert"
+                        />
+                    </a>
 
-                    <div class="flex items-center gap-6">
-                        <p class="text-[16px] leading-[24px]">
+                    <div
+                        class="flex w-full flex-col items-start gap-6
+                               md:w-auto md:flex-row md:items-center"
+                    >
+                        <p
+                            class="max-w-[230px] text-[16px] leading-[24px]
+                                   md:text-right"
+                        >
                             Daj znać, co możemy dla Ciebie zrobić!
                         </p>
 
-                        <BaseButton>
+                        <BaseButton
+                            class="w-full whitespace-nowrap md:w-auto"
+                        >
                             Skontaktuj się z nami
                         </BaseButton>
                     </div>
                 </div>
 
-                <div class="border-t border-[#F5F0EC]/70 pt-12">
-                    <div class="flex items-start justify-between">
+                <div class="h-px w-full bg-[#F5F0EC]/70"></div>
+
+                <div class="pt-10 md:pt-12">
+                    <div
+                        class="flex flex-col items-center gap-10 text-center
+                            md:flex-row md:items-start md:justify-between md:text-left"
+                    >
                         <nav
                             aria-label="Linki w stopce"
-                            class="flex items-center gap-12 text-[14px] leading-[21px]"
+                            class="flex flex-col items-center gap-4
+                                text-[14px] leading-[21px]
+                                md:flex-row md:items-center md:gap-12"
                         >
                             <a href="#kontakt" class="transition hover:opacity-70">
                                 Kontakt
@@ -50,21 +75,33 @@ import adRespactLogo from '../../assets/images/adrespect.svg'
                             </a>
                         </nav>
 
-                        <div class="flex items-center gap-12 text-[14px] leading-[21px]">
-                            <a href="tel:000000000" class="transition hover:opacity-70">
-                            000-000-000
+                        <div
+                            class="flex flex-col items-center gap-3
+                                text-[14px] leading-[21px] text-center
+                                md:items-end md:text-right
+                                lg:flex-row lg:items-center lg:gap-12"
+                        >
+                            <a
+                                href="tel:000000000"
+                                class="transition hover:opacity-70"
+                            >
+                                000-000-000
                             </a>
 
                             <a
-                            href="mailto:giarddesign@kontakt.pl"
-                            class="transition hover:opacity-70"
+                                href="mailto:giarddesign@kontakt.pl"
+                                class="transition hover:opacity-70"
                             >
-                            giarddesign@kontakt.pl
+                                giarddesign@kontakt.pl
                             </a>
                         </div>
                     </div>
 
-                    <div class="mt-[104px] flex items-center justify-between text-[14px] leading-[21px]">
+                    <div
+                        class="mt-16 flex flex-col items-center gap-5
+                            text-[14px] leading-[21px] text-center
+                            md:mt-[104px] md:flex-row md:justify-between md:text-left"
+                    >
                         <p>
                             Prawa zastrzeżone © 2022
                         </p>
@@ -72,10 +109,10 @@ import adRespactLogo from '../../assets/images/adrespect.svg'
                         <div class="flex items-center gap-2">
                             <span>made by</span>
 
-                            <img 
-                            :src="adRespactLogo" 
-                            alt="adRespect"
-                            class="h-[23px] w-[113px]"
+                            <img
+                                :src="adRespectLogo"
+                                alt="adRespect"
+                                class="h-[23px] w-auto"
                             />
                         </div>
                     </div>
